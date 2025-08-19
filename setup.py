e6adfbbccef440eb7bf57d75c545e9e33c01729d
PrasanthNimmala0 <141349117+rohit7234@users.noreply.github.com>
2025-08-19 23:42:07 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages , setup
+
+setup(
+    name='Generative Ai Project',
+    version='0.0.0',
+    author='Prasanth',
+    author_email='tirupati.22bce20385@vitapstudent.ac.in',
+    packages=find_packages(),
+    install_requires = []
+    
+    )
